56a6d25bf1d5a104a58d2d9f33d732f0a1d9368d
yodetaher <162877864+yodetaher@users.noreply.github.com>
2024-04-05 17:48:39 +0300

    Add files via upload

---
+++ moayadtaher.html/javascript.js
@@ -0,0 +1,16 @@
+function sayHello() {
+    alert('Hello, world!');
+}
+function sayhello2()
+{
+    alert( " happy eid and i wish to you good vecation teacher ")
+}
+function sayheloo3() {
+    alert("Teachers plant the seeds of knowledge that grow forever") 
+}
+function sayhello4() {
+    alert("Teaching is the profession that teaches all the other professions") 
+}
+function syahello5() {
+    alert(" in the end may allh bless you teacher and portects your family and reawrds you helthy inshallah ' " ) 
+}
